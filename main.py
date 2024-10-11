8eb53d4a2e8a6d98bc5b7162b8629fc205195b52
Yaroslav552 <boywithoutmood@gmail.com>
2024-10-11 14:18:16 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ def print_params(a = 1, b = 'строка', c = True):
     print(a, b, c)
 
 
-
+print_params(a=25, b='хи - хи', c=False)
+print_params()
+print_params(b = 25)
+print_params(c = [1,2,3])
 values_list = [1, 'у меня др', False]
 values_dict = {'a': 5, 'b': 'cg go', 'c': True}
 values_list_2 = [54.32, 'Строка' ]
