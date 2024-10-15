f78562122e3edcce6026977b560b9931424561c7
Yaroslav552 <boywithoutmood@gmail.com>
2024-10-15 18:22:10 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-def single_root_words(root_word, *other_words):
-    same_words = []
-    for i in other_words:
-        if root_word.lower() in i.lower() or i.lower() in root_word.lower():
-            same_words.append(i)
-    return same_words
+def get_multiplied_digits(number):
+    str_number = str(number)
+    first = int(str_number[0])
+    if len(str_number) == 1:
+        return first
+    else:
+        return first * get_multiplied_digits(int(str_number[1:]))
 
-result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
-result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
-print(result1)
-print(result2)
+
+result = get_multiplied_digits(40203)
+print(result)
+
+
+
+
 
