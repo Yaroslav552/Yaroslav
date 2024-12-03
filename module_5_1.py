a0e85f703c839bdc6613e60bd11535b4658f557b
Yaroslav552 <boywithoutmood@gmail.com>
2024-12-03 18:54:18 +0300

    Add files via upload

--- module_5_1.py
+++ module_5_1.py
@@ -2,21 +2,14 @@ from zoneinfo import reset_tzpath
 
 
 class House:
+    houses_history = []
+    def __new__(cls, *args, **kwargs):
+        cls.houses_history.append(args[0])
+        print(cls.houses_history)
+        return super().__new__(cls)
     def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
-    def go_to(self, new_floor):
-        if 1 <= new_floor <= self.number_of_floors:
-            for floor in range(1, new_floor + 1):
-                print(f'{floor}')
-            else:
-                print('Такого этажа не существует')
-    def __len__(self):
-        return self.number_of_floors
-
-    def __eq__(self, other):    # 1
-        if isinstance(other.number_of_floors, int) and isinstance(other, House):
-            return self.number_of_floors == other.number_of_floors
     def __lt__(self, other):
         return self.number_of_floors < other.number_of_floors
     def __le__(self, other):
@@ -27,43 +20,22 @@ class House:
         return self.number_of_floors >= other.number_of_floors
     def __ne__(self, other):
         return self.number_of_floors != other.number_of_floors
-    def __add__(self, value):
-        if isinstance(value, int):
-            self.number_of_floors = self.number_of_floors + value
-        return self
-    def __radd__(self, value):
-        return self.__add__(value)
 
-    def __iadd__(self, value):
-        if isinstance(value, int):
-            self.number_of_floors += value
-        return self
-    def __str__(self):
-        title = str(f'Название: {self.name}, кол-во этажей: {self.number_of_floors}')
-        return title
+
+    def __del__(self):
+        print(self.name, ' снесён, но он останется в истории')
 
 h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
 h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
 
-print(h1)
-print(h2)
+# Удаление объектов
+del h2
+del h3
 
-print(h1 == h2) # __eq__
-
-h1 = h1 + 10 # __add__
-print(h1)
-print(h1 == h2)
-
-h1 += 10 # __iadd__
-print(h1)
-
-h2 = 10 + h2 # __radd__
-print(h2)
-
-print(h1 > h2) # __gt__
-print(h1 >= h2) # __ge__
-print(h1 < h2) # __lt__
-print(h1 <= h2) # __le__
-print(h1 != h2) # __ne__
+print(House.houses_history)
 
 
